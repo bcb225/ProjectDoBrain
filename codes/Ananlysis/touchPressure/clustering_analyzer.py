ef0c9e23d907e20eb94d9046dc3a2465d6b9d417
bcb225 <bcb225@gmail.com>
2018-07-29 19:52:51 +0900

    Plt plot done.

--- codes/Ananlysis/touchPressure/clustering_analyzer.py
+++ codes/Ananlysis/touchPressure/clustering_analyzer.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+from matplotlib import rc
+import matplotlib.pyplot as plt
 class ClusteringAnalyzer:
     def __init__(self, label_array, data_array):
         self.label_array = label_array
@@ -16,9 +18,29 @@ class ClusteringAnalyzer:
         self.data_matrix_2d = reshaped_arr.T
 
     def do_kmeans(self,n_clusters):
-        X = np.array(self.data_matrix_2d)
-        kmeans = KMeans(n_clusters=n_clusters,random_state=0).fit(X)
-        return kmeans.labels_
+        self.X = np.array(self.data_matrix_2d)
+        self.kmeans = KMeans(n_clusters=n_clusters,random_state=0).fit(self.X)
+        return self.kmeans.labels_
 
-    def draw_png(self):
-        pass
+    def draw_png(self,suptitle,subtitle_1,subtitle_2,xlabel,file_path):
+        #rc('text',usetex = True)
+        #rc('font',family = 'serif')
+
+        #Kernel Density Estimation
+        data_ser = pd.Series(self.data_array_1d)    #set Series
+        ax1 = plt.subplot(211)  #ready first subplot (ax1 is axis)
+        plt.title(subtitle_1)   #set subtitle
+        plt.xlabel(xlabel)      #set xlabel
+        data_ser.plot.kde()     #calculate kernal density estimation
+        
+        #Clustering Scatter
+        plt.subplot(212,sharex = ax1)   #reday second subplot with shared axis-X
+        plt.title(subtitle_2)           #set subtitle
+        plt.scatter(self.X[:,0],self.X[:,1],c=self.kmeans.labels_)  #scatter clustering result
+        plt.xlabel(xlabel)              #set xlabel
+        plt.yticks([])                  #remove yticks
+
+        plt.subplots_adjust(hspace = 0.8)   #adjust vertical white space
+
+        plt.suptitle(suptitle)  #set superior title
+        plt.savefig(file_path)  #save figure as file_path
--- codes/Ananlysis/touchPressure/main.py
+++ codes/Ananlysis/touchPressure/main.py
@@ -20,7 +20,7 @@ handler = DataFrameHandler(loaded_df)
 
 sorted_unique_index = handler.get_unique_index()
 
-selected_df_by_index = handler.get_rows_by_index([1,6,0])
+selected_df_by_index = handler.get_rows_by_index([1,6,1])
 
 true_answer_df = handler.filter_rows_by_correct(selected_df_by_index,True)
 false_anwer_df = handler.filter_rows_by_correct(selected_df_by_index,False)
@@ -32,3 +32,9 @@ proportion_person_array, proportion_data_array = handler.join_df_and_divide_pres
 
 proportion_pressure_analyzer = ClusteringAnalyzer(proportion_person_array,proportion_data_array)
 print(proportion_pressure_analyzer.do_kmeans(2))
+
+#proportion_pressure_analyzer.draw_png(r'$\frac{\text{Mean Wrong Pressure}}{\text{Mean Ture Pressure}}$','test.png')
+proportion_pressure_analyzer.draw_png(
+    suptitle='',subtitle_1='Kernel Density Estimation',subtitle_2='KMeans Clustering'
+    ,xlabel='Mean Wrong Pressure / Mean Correct Pressure',file_path ='test.png'
+    )
