c6378f287dd4fe1d060b6c8e1ebd588800da7d2c
bcb225 <bcb225@gmail.com>
2018-08-16 17:24:01 +0900

    Automatic module.

--- codes/Ananlysis/touchPressure/Association/association_analyzer.py
+++ codes/Ananlysis/touchPressure/Association/association_analyzer.py
@@ -7,11 +7,12 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from scipy import stats
 from itertools import combinations
 
+from csv_handler import CsvHandler
 import matplotlib.pyplot as plt
 import json
 
 class AssociationAnalyzer:
-    def __init__(self, df_object):
+    def __init__(self, df_object, index_list, file_path):
         self.df_object = df_object
         self.df_dict = {
             'A' : df_object.loc[df_object['level'] == 'A'],
@@ -25,9 +26,24 @@ class AssociationAnalyzer:
         }
         self.level_list = ['A', 'B', 'C']
         
+        self.level_info_dict_list = []
+        for level in self.level_list:
+            level_info_dict = {
+                'group' : level,
+                'count' : self.df_dict[level].shape[0]
+            }
+            self.level_info_dict_list.append(level_info_dict)
+        print(self.level_info_dict_list)
+        self.index_str = str(index_list[0]) + '_' + str(index_list[1]) + '_' +str(index_list[2])
+
+        self.png_path = file_path + '/png/' + self.index_str + '.png'
+        self.csv_ttest_path = file_path + '/csv/ttest/' + self.index_str + '.csv'
+        self.csv_info_path = file_path + '/csv/info/' + self.index_str + '.csv'
+
+        self.t_stat_header_list = ['group 1', 'group 2', 't-statistic', 'p-value']
+        self.level_info_header_list = ['group', 'count']
     def t_test(self):
         self.statistic_dict_list = []
-        self.pvalue_dict_list = []
         for combo in combinations(self.level_list,2):
             
             combo_list = list(combo)
@@ -36,22 +52,18 @@ class AssociationAnalyzer:
                 self.touchPressure_dict[combo_list[1]]
                 )
             temp_statistic_dict = {
-                combo_list[0] : {
-                    combo_list[1] : statistic
+                'group 1' : combo_list[0],
+                'group 2' : combo_list[1],
+                't-statistic' : statistic,
+                'p-value' : pvalue
                 }
-            }
-            temp_pvalue_dict = {
-                combo_list[0] : {
-                    combo_list[1] : pvalue
-                }
-            }
             self.statistic_dict_list.append(temp_statistic_dict)
-            self.pvalue_dict_list.append(temp_pvalue_dict)
-        
+
+        return self.statistic_dict_list
                 
     def draw_box_plot(self):
         print(self.statistic_dict_list)
-        print(self.pvalue_dict_list)
+        
         np_list = []
         xtick_list = []
         for level in self.level_list:
@@ -65,13 +77,29 @@ class AssociationAnalyzer:
         ax.set_xticklabels(xtick_list)
         ax.set_xlabel('Group')
         ax.set_ylabel('Mean Touch Pressure')
-        ax.set_title('Box Plot of First Content')
+        ax.set_title(self.index_str)
         group_A = 0
         group_B = 1
         group_C = 2
-        ax.text(
-            x=(group_A+group_B)*0.5,
-            y=y+2,
-            s='ss'
-            )
-        plt.show()
+
+        plt.savefig(self.png_path)
+
+        plt.clf()
+    
+    def save_statistics_as_csv(self):
+        statistic_csv_handler = CsvHandler(
+            filepath = self.csv_ttest_path,
+            header_list = self.t_stat_header_list
+        )
+        statistic_csv_handler.dict_to_csv(
+            dict_list = self.statistic_dict_list
+        )
+
+    def save_record_info_as_csv(self):
+        info_csv_handler = CsvHandler(
+            filepath = self.csv_info_path,
+            header_list = self.level_info_header_list
+        )
+        info_csv_handler.dict_to_csv(
+            dict_list = self.level_info_dict_list
+        )
--- codes/Ananlysis/touchPressure/Association/main.py
+++ codes/Ananlysis/touchPressure/Association/main.py
@@ -4,13 +4,14 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 from csv_loader import CsvLoader
 from association_analyzer import AssociationAnalyzer
+
 import df_handler
 def parse_commands(argv):
     from optparse import OptionParser
     parser = OptionParser('"')
     parser.add_option('-d', '--dragFile', dest='drag_file')
     parser.add_option('-u', '--userFile', dest='user_file' )
-    parser.add_option('-o', '--outputFile', dest='output_file')
+    parser.add_option('-o', '--outputFilePath', dest='output_file_path')
     options, otherjunk = parser.parse_args(argv)
     return options
 
@@ -32,26 +33,42 @@ plus_one_removed_df = df_handler.remove_one_pressure(minus_one_removed_df)
 sorted_unique_index = df_handler.get_unique_index(plus_one_removed_df)
 
 #get first content, game index of drag data
-first_index_list = sorted_unique_index[0]
+#first_index_list = sorted_unique_index[6]
+for index_list in sorted_unique_index:
+    
+    #select first game as interest
+    selected_drag_df_by_index = df_handler.get_rows_by_index(plus_one_removed_df,index_list)
 
-#select first game as interest
-selected_drag_df_by_index = df_handler.get_rows_by_index(plus_one_removed_df,first_index_list)
+    #group by personId and get mean touch pressure of the game
+    group_mean_touch_pressure_df = df_handler.group_mean_touch_pressure(selected_drag_df_by_index)
 
-#group by personId and get mean touch pressure of the game
-group_mean_touch_pressure_df = df_handler.group_mean_touch_pressure(selected_drag_df_by_index)
+    #clean mean touch pressure (grouped) as df_source (remove 1.0000 mean touchPressure of df_source)
+    #NOT drag_handler's df_object
+    plus_one_removed_grouped_df = df_handler.remove_one_pressure(group_mean_touch_pressure_df)
 
-#clean mean touch pressure (grouped) as df_source (remove 1.0000 mean touchPressure of df_source)
-#NOT drag_handler's df_object
-plus_one_removed_grouped_df = df_handler.remove_one_pressure(group_mean_touch_pressure_df)
+    #join user level table and drag data table with key person_id
+    joined_by_person_id_df = df_handler.join_df_by_key(loaded_user_df,plus_one_removed_grouped_df,'person_id')
 
-#join user level table and drag data table with key person_id
-joined_by_person_id_df = df_handler.join_df_by_key(loaded_user_df,plus_one_removed_grouped_df,'person_id')
+    
 
-#create AssociationAnalyzer module
-association_analyzer = AssociationAnalyzer(joined_by_person_id_df)
+    #create AssociationAnalyzer module
+    try:
+        association_analyzer = AssociationAnalyzer(
+            df_object = joined_by_person_id_df,
+            index_list=index_list,
+            file_path = options.output_file_path
+            )
 
-#t_test
-association_analyzer.t_test()
+        #t_test
+        association_analyzer.t_test()
 
-#draw box plot
-association_analyzer.draw_box_plot()
+        #draw box plot
+        association_analyzer.draw_box_plot()
+
+        association_analyzer.save_statistics_as_csv()
+
+        association_analyzer.save_record_info_as_csv()
+        print(index_list,'OK')
+    except:
+        print(index_list,'EXCEPTION')
+        pass
