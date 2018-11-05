fd6d951703058fd1a6b97427866bd6b109d6ea16
bcb225 <bcb225@gmail.com>
2018-11-05 15:30:22 +0900

    save json for drag data.

--- codes/Firebase/Drag/main.py
+++ codes/Firebase/Drag/main.py
@@ -35,12 +35,15 @@ rest_handler = RestHandler(options.mobile_os)
 json_handler = JsonHandler()
 csv_handler = CsvHandler(filepath=options.drag_file,header_list=header_list)
 
+f = open(options.json_file, 'w')
 for person_id in person_list:
     try:
         json_result = rest_handler.get_json_by_person_id(person_id)
+        f.write(json_result+'\n')
         result_dict_list = json_handler.json_to_dict_list(json_result,person_id)
         csv_handler.dict_to_csv(dict_list=result_dict_list)
         print(person_id,len(result_dict_list))
     except:
         print('EXCEPTION',person_id)
         pass
+f.close()
