ee00852eeeaf40b0fa4a636e0dbf3ecfbbc26acb
bcb225 <bcb225@gmail.com>
2018-11-05 15:31:30 +0900

    save json file for survey data.

--- codes/Firebase/Survey/main.py
+++ codes/Firebase/Survey/main.py
@@ -11,10 +11,12 @@ def parse_commands(argv):
     parser = OptionParser('"')
     parser.add_option('-s', '--surveyFile', dest='survey_file')
     parser.add_option('-m', '--mobileOS', dest='mobile_os')
+    parser.add_option('-j', '--jsonFile', dest='json_file')
     options, otherjunk = parser.parse_args(argv)
     return options
 
 options = parse_commands(sys.argv[1:])
+f = open(options.json_file, 'w')
 
 header_list = [
     'person_id', 'diagnosedDisease'
@@ -25,5 +27,9 @@ json_handler = JsonHandler()
 csv_handler = CsvHandler(filepath=options.survey_file,header_list=header_list)
 
 json_result = rest_handler.get_survey_data()
+f.write(json_result+'\n')
+
 result_dict_list = json_handler.json_survey_data_to_dict(json_result)
 csv_handler.dict_to_csv(dict_list = result_dict_list)
+
+f.close()
