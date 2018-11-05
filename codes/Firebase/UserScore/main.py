4413c4670d9a62ccdfec27a594bf8ae4aa2f3e2c
bcb225 <bcb225@gmail.com>
2018-11-05 15:28:39 +0900

    save json_file for userScore.

--- codes/Firebase/UserScore/main.py
+++ codes/Firebase/UserScore/main.py
@@ -12,6 +12,7 @@ def parse_commands(argv):
     parser.add_option('-u', '--userScoreFile', dest='user_score_file')
     parser.add_option('-p', '--personFile', dest='person_file')
     parser.add_option('-m', '--mobileOS', dest='mobile_os')
+    parser.add_option('-j', '--jsonFile', dest='json_file')
     options, otherjunk = parser.parse_args(argv)
     return options
 
@@ -27,9 +28,16 @@ header_list = ['person_id', 'level', 'game_level','clear_date_time','Memory','Ve
 rest_handler = RestHandler(mobile_os=options.mobile_os)
 json_handler = JsonHandler()
 csv_handler = CsvHandler(filepath=options.user_score_file,header_list=header_list)
+
+f = open(options.json_file, 'w')
 i = 0
 content_num = 0
 for person_id in person_list:
+    if i == 100:
+        break
     json_result = rest_handler.get_user_score_data_by_person_id(person_id)
+    f.write(json_result+'\n')
     result_dict_list = json_handler.json_user_score_data_to_dict_list(json_result,person_id, content_num)
     csv_handler.dict_to_csv(dict_list=result_dict_list)
+    i = i +1
+f.close()
