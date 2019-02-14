937852f9f2be55d204fdd0117eb017a8a2063ad4
bcb225 <bcb225@gmail.com>
2019-02-14 15:22:43 +0900

    Collector for user score by date of asan people.

--- codes/Firebase/UserScoreByDate/only_asan.py
+++ codes/Firebase/UserScoreByDate/only_asan.py
@@ -16,7 +16,7 @@ from csv_handler import CsvHandler
 def parse_commands(argv):
     from optparse import OptionParser
     parser = OptionParser('"')
-    parser.add_option('-o', '--dragFile', dest='drag_file')
+    parser.add_option('-s', '--scoreFile', dest='score_file')
     parser.add_option('-j', '--jsonFile', dest='json_file')
     parser.add_option('-a', '--asanFile', dest='asan_file')
     options, otherjunk = parser.parse_args(argv)
@@ -24,19 +24,16 @@ def parse_commands(argv):
 
 options = parse_commands(sys.argv[1:])
 
-header_list = ["person_id","deviceModel","deviceName","dpi", "updateDateTime", "screenHeight", "screenWidth",
-                "level", "contentIndex", "questionIndex", "derivedQuestionIndex",
-                "questionManagerCategory", "dragDataSetCreationDateTime",
-                "dragDataCreationDateTime", "isOnCorrectAnswer", 
-                "posX", "posY", "touchPressure"
-                ]
-
+header_list = [
+    "person_id", "level", "chapterIndex", "contentIndex", "questionIndex",
+    "derivedIndex", "duration","point", "clearDateTime", "incorrectAnswerCount"
+    ]
 rest_handler = RestHandler()
 json_handler = JsonHandler()
 csv_handler = CsvHandler(filepath=options.drag_file,header_list=header_list)
 
 #f = open(options.json_file, 'w')
-date_list_json = rest_handler.get_json_of_date_list()
+date_list_json = rest_handler.get_score_json_of_date_list()
 date_list = json_handler.json_to_date_list(date_list_json)
 survey_person_list = []
 asan = pd.read_csv(options.asan_file)
@@ -52,15 +49,15 @@ for idx, date in enumerate(date_list):
         continue
     print('[{}], ({}/{}) Now Collecting'.format(date,idx+1,len(date_list)))
     for mobile_os in ('iOS', 'Android'):
-        person_list_json = rest_handler.get_json_of_person_id_by_date(date,mobile_os)
+        person_list_json = rest_handler.get_score_json_of_person_id_by_date(date,mobile_os)
         person_list = json_handler.json_to_person_list(person_list_json,mobile_os)
         all_person_set = set(person_list)
         updated_person_set = all_person_set.intersection(survey_person_set)
         updated_person_list = list(updated_person_set)
         print('\t\tAll {} users : {}, Survey {} users {}'.format(mobile_os,len(person_list),mobile_os,len(updated_person_list)))
         for person_id in updated_person_list:
-            drag_data_json = rest_handler.get_json_by_date_and_person_id(date,person_id,mobile_os)
-            temp_dict_list = json_handler.json_to_dict_list(json_source = drag_data_json,person_id =person_id)
+            score_data_json = rest_handler.get_score_json_by_date_and_person_id(date,person_id,mobile_os)
+            temp_dict_list = json_handler.score_json_to_dict_list(json_source = score_data_json,person_id =person_id)
             #result_dict_list += temp_dict_list
             csv_handler.dict_to_csv(temp_dict_list)
     #del result_dict_list
